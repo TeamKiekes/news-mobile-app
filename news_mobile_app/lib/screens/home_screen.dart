bfaf4370d9a6ba65a2d526892d63dc1223ab8476
bollend <dylanbollendb@gmail.com>
2021-03-03 21:42:30 +0100

    Category container added

--- news_mobile_app/lib/screens/home_screen.dart
+++ news_mobile_app/lib/screens/home_screen.dart
@@ -9,16 +9,56 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text("My"),
-        Text(
-          "News",
-          style: TextStyle(color: Colors.blue),
-        )
-      ],
-    )));
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("My"),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.blue),
+            )
+          ],
+        ),
+        leading: Icon(Icons.menu),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: <Widget>[
+                // Categories
+                Container(
+                  height: 70,
+                  child: ListView.builder(
+                    itemCount: categories.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return CategoryTile(
+                        imageUrl: categories[index.imageUrl,
+                        categoryName: categories[index].categoryName,
+                      );
+                    }),
+                )
+              ],
+            )
+            
+            )),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final String imageUrl, categoryName;
+  CategoryTile({this.imageUrl, this.categoryName});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
   }
 }
